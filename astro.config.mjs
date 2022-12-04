4041e00d8c30105bc27b185bd09c68482d4cd856
ibulog <charo.little@gmail.com>
2022-12-04 15:13:48 +0900

    remove Image plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ibulog.blog',
-  integrations: [mdx(), sitemap(), image()],
+  integrations: [mdx(), sitemap()],
   output: "server",
   adapter: vercel()
 });
