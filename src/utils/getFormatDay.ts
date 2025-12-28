5897d2660d2cb9a3e1e0bf689660e8e58491ac29
ibulog <ibulog76@gmail.com>
2025-12-28 19:02:15 +0900

    Job画面の日は省略する

--- src/utils/getFormatDay.ts
+++ src/utils/getFormatDay.ts
@@ -1,7 +1,14 @@
-export default function getFormatDay(date: Date) {
+export function getFormatDay(date: Date) {
   return date.toLocaleDateString('ja-JP', {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
+  }).replace(/\//g, '-');
+}
+
+export function getFormatMonthYear(date: Date) {
+  return date.toLocaleDateString('ja-JP', {
+    year: 'numeric',
+    month: '2-digit',
   }).replace(/\//g, '-');
 }
