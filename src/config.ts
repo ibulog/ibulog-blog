9727a9002cf428a2895f330ba63ddc030c826fd0
ibulog <ibulog76@gmail.com>
2025-12-28 17:59:34 +0900

    404ページの画像も最適化を有効にする

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,13 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 import iconImage from './assets/icon.jpg';
+import notFoundImage from './assets/404.jpg';
 
 export const SITE_TITLE = 'いぶろぐ雑記';
 export const SITE_DESCRIPTION = 'ITとか、UXライティングとか、車とか';
 export const SITE_BASE_URL = 'https://ibulog.me';
 export const ICON_IMAGE = iconImage;
+export const NOT_FOUND_IMAGE = notFoundImage;
 
 export const NAV_ITEMS = [
 	{ label: 'About', href: '/' },
